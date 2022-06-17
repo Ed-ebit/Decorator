7a8694d759717198d643a6851826daefa4823619
KMV <karl-martin.vogel@tc.de>
2022-06-17 15:48:52 +0200

    components und decorators feddich

--- Decorator/Decorator/AdditionIce.cs
+++ Decorator/Decorator/AdditionIce.cs
@@ -7,8 +7,20 @@ namespace Decorator.Decorator
 {
     public class AdditionIce : IAdditions
     {
-        public ICoffeeBar CoffeeBar { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ICoffeeBar CoffeeBar {get; set;}
+        public AdditionIce(ICoffeeBar coffeeBar)
+        {
+            CoffeeBar = coffeeBar;
+        }
 
-        public void MakeCoffee() { }
+        public void MakeCoffee() {
+            CoffeeBar.MakeCoffee();
+            Console.WriteLine("Ice has been added, new function triggered:\n" +
+                "THE WATERING :D " + watering());
+        }
+        public string watering() {
+            return "\n your drink is being watered because u like to have less taste than other ppl!";
+        }
+
     }
 }
